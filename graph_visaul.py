c22fd84ce31475152edd34f2e5ccadf25f07da28
niris <niris@users.noreply.github.com>
2023-02-26 01:35:24 +0700

    extract graph from .csv file

--- graph_visaul.py
+++ graph_visaul.py
@@ -1,3 +1,5 @@
+import csv
+import os
 from neo4j import GraphDatabase
 
 class GraphConnect:
@@ -27,13 +29,14 @@ class GraphConnect:
                             YIELD rel
                             RETURN a.text + '  ' + b.text
                             """, tail_type=tail['type'],tail_text=tail['text'],head_type=head['type'],head_text=head['text'],rel=rel)
-            return result.single()[0]
-
+            return result.single()[0]    
 
 if __name__ == "__main__":
     greeter = GraphConnect("bolt://localhost:7687", "neo4j", "mypassword")
-    tail = {"type":"Person","text":"Jame"}
-    head = {"type":"Place","text":"Osaka"}
-    res= greeter.create_and_return_triplet(tail,head,"Like")
-    print(res)
+    with open(os.path.join("input","graph.csv"),'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            tail = {"type":row[1],"text":row[0]}
+            head = {"type":row[3],"text":row[2]}
+            res=greeter.create_and_return_triplet(tail,head,row[4])
     greeter.close()
